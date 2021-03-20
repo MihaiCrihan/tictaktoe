1ea066c23ad6d8ff92ad41bf6a0375236c3d239d
Mihai <haos-sah@mail.ru>
2021-03-20 18:52:55 +0200

    push

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,107 @@
-<template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
-</template>
 
 <script>
 export default {
-  name: 'HelloWorld',
+  data: () => ({
+    counter: 0,
+    matrix: [
+      [0, 0, 0],
+      [0, 0, 0],
+      [0, 0, 0]
+    ]
+  }),
   props: {
     msg: String
+  },
+  methods: {
+    getCoordinate(rowIndex,columnIndex) {
+      this.counter = this.counter + 1
+      console.log(rowIndex,columnIndex, this.counter)
+      if (this.counter % 2 === 0) {
+        this.matrix[rowIndex].splice(columnIndex,1,1)
+      } else if (this.counter % 2 !== 0) {
+        this.matrix[rowIndex].splice(columnIndex,1,2)
+      }
+      console.log(this.matrix)
+      // console.log(this.matrix[2].slice())
+      // for (let i in this.matrix) {
+      //   for (let j in this.matrix[i].slice()[j]) {
+      //       console.log(j)
+      //     //  if (this.matrix[2].slice[j] === 2){
+      //     //   console.log("circle")
+      //     // } else {
+      //     //   console.log('cross')
+      //     // }
+      //   }
+      // }
+    }
+  },
+  mounted() {
   }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+
+    <div class="tictaktoe">
+      <div v-for="(row, rowIndex) in matrix" :key="rowIndex" class="field">
+        <div v-for="(column, columnIndex) in row" :key="columnIndex">
+          <div
+              @click="getCoordinate(rowIndex,columnIndex)"
+              :class="column === 1 ? 'cross' : column === 2 ? 'circle' : 'no-clicked'"
+          >
+          </div>
+        </div>
+        <br>
+      </div>
+    </div>
+  </div>
+</template>
+
 <style scoped>
+.clicked {
+
+}
+.cross {
+  background: url("../assets/cross.svg") no-repeat center;
+  background-size: 30px 30px;
+  pointer-events: none;
+}
+.circle {
+  background: url("../assets/circle.svg") no-repeat center;
+  background-size: 30px 30px;
+  pointer-events: none;
+}
+.no-clicked {
+}
 h3 {
   margin: 40px 0 0;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+.field div{
+  width: 40px;
+  border: 1px solid #404D59;
+  min-height: 40px;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+.field div:first-child {
+  border-top: none;
 }
-a {
-  color: #42b983;
+.field div:nth-child(3) div,
+.field div:nth-child(3) {
+  border-bottom: none;
+}
+.field:last-child div {
+  border-right: none;
+  border-left: none;
+}
+.field:first-child div {
+  border-right: none;
+  border-left: none;
+}
+
+.tictaktoe {
+  display: flex;
+  justify-content: center;
+  margin: 0 auto;
 }
 </style>
